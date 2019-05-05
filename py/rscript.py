327b38943e529e7daa4b397f6b5f66b0f8bff2d5
ashlinrichardson <richardson.ashlin@gmail.com>
2019-05-05 15:14:58 -0700

    update
    update

--- py/rscript.py
+++ py/rscript.py
@@ -5,6 +5,6 @@ if len(sys.argv) < 2:
     err("rscript.py: usage: [name of R file to run] [first parameter for R script] ...[last parameter for R script]")
 
 # convert to a path that works!
-cmd = os.popen("(cygpath -d '/cygdrive/c/Program Files/R/R-3.5.2/bin/Rscript.exe')").read().strip()
+cmd = os.popen("(cygpath -d '/cygdrive/c/Program Files/R/R-3.5.3/bin/Rscript.exe')").read().strip()
 a = os.system(cmd + " " + " ".join(sys.argv[1:]))
 
