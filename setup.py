2d7045535d76aca0ee486b6a594c6cf4a1b2067a
ashlinrichardson <richardson.ashlin@gmail.com>
2019-07-13 23:05:58 -0700

    update for multicore compilation
    update for multicore compilation

--- setup.py
+++ setup.py
@@ -1,56 +1,35 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 import os
 import sys
-import py.misc
+from py.misc import *
+from py.ansicolor import *
+
+job_file = open("compile_jobs.sh", "wb")
+
+def add_job(cmd):
+    job_file.write((cmd + "\n").encode())
 
 # need to be able to detect python, R, and vim (if exist)
 # need to do the wrapping (as we did for py) for R programs, too!
 # CHECK ON writing (and executing) the bash_profile..
 # 20190401 this program now compatible with python3
 
-def err(m):
-    print("Error: " + m)
-    sys.exit(1)
-
-sys_s = os.popen("uname -a").read().strip().lower()
-sys_n = None
-
-if sys_s[0:6] == 'cygwin':
-    sys_n = 'cygwin'
-elif sys_s[0:5] == 'mingw':
-    sys_n = 'mingw'
-elif sys_s[0:5] == 'linux':
-    sys_n = 'linux'
-else:
-    err("system type: " + sys_s.split()[0] + " not yet supported")
+sys_s, sys_n = os.popen("uname -a").read().strip().lower(), None
+if sys_s[0:6] == 'cygwin': sys_n = 'cygwin'
+elif sys_s[0:5] == 'mingw': sys_n = 'mingw'
+elif sys_s[0:5] == 'linux': sys_n = 'linux'
+else: err("system type: " + sys_s.split()[0] + " not yet supported")
 
 file_dir = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))  # where the file is
 pwd = os.popen("pwd").read().strip()
 
-if sys_n == "cygwin":
-    pwd = os.popen("cygpath -w -a ./").read().strip()
+if sys_n == "cygwin": pwd = os.popen("cygpath -w -a ./").read().strip()
 
 work_dir = os.path.abspath(pwd)
+if not file_dir == work_dir: err("file dir: " + file_dir + "\nwork_dir: " + work_dir + "\nrun setup.py from the directory it's in")
 
-if not file_dir == work_dir:
-    err("file dir: " + file_dir + "\nwork_dir: " + work_dir + "\nmust run setup.py from the directory it lives in")
-
-build = None
-if len(sys.argv) > 1:
-    build = sys.argv[1]
-
+build = sys.argv[1] if len(sys.argv) > 1 else None # if len(sys.argv) > 1: build = sys.argv[1]
 user_name = os.popen("whoami").read().strip()
-
-def err(m):
-    print("Error: " + m)
-    sys.exit(1)
-
-def run(c):
-    print('running:' + str(c))
-    a = os.system(c)
-    if(a!=0):
-        err("command failed: " + c)
-
 
 # need to actually adjust these for the python / R vim paths found..
 profile = ['alias vim="/cygdrive/c/Program\ Files\ \(x86\)/Vim/vim81/vim.exe"',
@@ -67,33 +46,34 @@ profile = ['alias vim="/cygdrive/c/Program\ Files\ \(x86\)/Vim/vim81/vim.exe"',
 compiled = {}
 
 def compile(f):
+    # print(KRED + f.strip('./') + KGRN)
+    global build, compiled
     ext = f.strip().split('.')[-1].strip()
     fn = f.split('/')[-1].strip().split('.')[0]
+    # print("fn", fn)
+    if ext == 'py' and build is not None and build != fn: return
 
     # don't compile helper funcs as a binary; no main()
-    if fn == "misc" or fn == "__init__" or fn == "wrap_py":
-        return
+    if fn == "misc" or fn == "__init__" or fn == "wrap_py": return
 
-    if not fn in compiled:
-        compiled[fn] = True
-    else:
-        return
+    if not fn in compiled: compiled[fn] = True
+    else: return
+
+    # print(KRED + f.strip('./') + KGRN)
+
+    cmds = ""
     if ext == 'c' or ext == 'cpp':
         cmd = 'g++ -w -O4 -march=native -flto -o ' + fn + '.exe ' + f
-        if ext == 'cpp':
-            cmd += ' cpp/misc.cpp'
+        if ext == 'cpp': cmd += ' cpp/misc.cpp'
         cmd += ' -lopengl32 -lglu32 -lgdi32'
         if (not build) or (fn == build):
-            print(cmd)
-            a = os.system(cmd)
+            print('\t' + cmd)
+            cmds += cmd # add_job(cmd) # a = os.system(cmd) # note: didn't use run because compiling might brea
 
     if ext == 'py':
-        if build != None:
-            if build != fn:
-                return
-
+        wrap_file = 'wrap-py_' + fn + '.cpp'
         # for py files, write a cpp wrapper so we can call from bin folder
-        cf = open('wrap_py.cpp', 'wb')
+        cf = open(wrap_file, 'wb') #'wrap_py.cpp', 'wb')
         lines = ['#include<stdlib.h>',
                  '#include<iostream>',
                  '#include<string>',
@@ -111,35 +91,26 @@ def compile(f):
                  '}']
         cf.write('\n'.join(lines).encode())
         cf.close()
-        cmd = 'g++ -w -O3 -o ' + fn + '.exe ' + ' wrap_py.cpp '
-        print(cmd)
-        a = os.system(cmd)
-        a = os.system("rm -f wrap_py.cpp")
-    cmd = "icacls " + fn + ".exe /grant everyone:F"
-    a = os.popen(cmd).read()
+        cmd = 'g++ -w -O3 -o ' + fn + '.exe ' + ' ' + wrap_file #wrap_py.cpp '
+        print('\t' + cmd)
+        cmds += cmd # a = os.system(cmd)
+        cmd =  ("rm -f " + wrap_file) #wrap_py.cpp")
+        cmds += "; " + cmd; # a = os.system(cmd)
+        #if os.path.exists(fn + ".exe"):
+        if(False):
+            cmd = "icacls " + fn + ".exe /grant everyone:F"
+            cmds += "; " + cmd # a = os.popen(cmd).read()
+
+    add_job(cmds)
 
 # find source files and compile
-cpp = os.popen('find ./ -name "*.cpp"').read().strip().split('\n')
-py = os.popen('find ./ -name "*.py"').read().strip().split('\n')
-c = os.popen('find ./ -name "*.c"').read().strip().split('\n')
-
-ci = 1
+cpp = os.popen('find ./cpp/ -name "*.cpp"').read().strip().split('\n')
+py = os.popen('find ./py/ -name "*.py"').read().strip().split('\n')
+c = os.popen('find ./c/ -name "*.c"').read().strip().split('\n')
 n = len(cpp) + len(py) + len(c)
 
-for f in py:
-    if build is None:
-        sys.stdout.write(str(ci) + "/" + str(n) + " ")
-    compile(f)
-    ci += 1
+for f in py: compile(f)
+for f in c: compile(f)
+for f in cpp: compile(f)
 
-for f in c:
-    if build is None:
-        sys.stdout.write(str(ci) + "/" + str(n) + " ")
-    compile(f)
-    ci += 1
-
-for f in cpp:
-    if build is None:
-        sys.stdout.write(str(ci) + "/" + str(n) + " ")
-    compile(f)
-    ci += 1
+job_file.close()
---
+++ setup.sh
@@ -0,0 +1,12 @@
+# 20190711
+echo setting up environment..
+source bash_profile
+echo building command list..
+/cygdrive/c/Program\ Files/Python35/python.exe setup.py > /dev/null
+echo compiling multicore compiler..
+g++ -w -O3 -o multicore.exe  wrap-py_multicore.cpp; rm -f wrap-py_multicore.cpp; icacls multicore.exe /grant everyone:F # compile_multicore.sh
+echo compiling utilities..
+multicore compile_jobs.sh
+echo setting permissions..
+icacls  .   /grant everyone:F  /t > /dev/null
+echo done
