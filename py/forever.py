aa046124dec83869f16ed59d4db0231b7a28d270
ashlinrichardson <richardson.ashlin@gmail.com>
2019-08-02 20:55:03 -0700

    update
    update

--- py/forever.py
+++ py/forever.py
@@ -5,7 +5,7 @@ from misc import *
 
 default = 10
 args = sys.argv
-delay, arg = default, args[1:]
+delay, arg = default, " ".join(args[1:])
 if len(args) > 1:
     try:
         delay = int(args[1])
@@ -13,11 +13,15 @@ if len(args) > 1:
     except:
         pass
 
-    cmd = ' '.join(arg)
-    print cmd
+    print "sys.argv", args
+    print "arg", arg
 
+    cmds = " ".join(arg).split(";")
+    print "cmds", cmds
     while(True):
-        a = os.system(cmd)
-        if a != 0:
-            pass
+        for c in cmds:
+            print("\texec(" + c + ")")
+            a = os.system(c)
+            if a != 0:
+                pass # could have a warning in here
         time.sleep(delay)
