af1ab4a0a2b5905a8bae161176e7a52887a3694a
ashlinrichardson <richardson.ashlin@gmail.com>
2019-08-02 21:04:31 -0700

    add support for nested commas (inside quotation marks)
    add support for nested commas (inside quotation marks)

--- cpp/misc.cpp
+++ cpp/misc.cpp
@@ -37,6 +37,71 @@ vector<string> split(string s){
   return ret;
 }
 
+void dbg(char c, int start_pos, int end_pos, str action, bool inside_quotes){
+  cout << "c[" << c << "] start [" << start_pos << "] end [" << end_pos << "] " << action << " " << (inside_quotes?str("inside"):str("outside")) << "]" <<endl;
+}
+
+/*
+#include"misc.cpp"
+int main(int argc, char ** argv){
+  str data("1,\",,2,3\",,,hello,12345,\"{12,12,12}\",word");
+  cout << "[" << data << "]" << endl;
+  cout << split(data) << endl;
+  vector<str> ss(split_special(data));
+  cout << "good:"<< ss << endl;
+  cout << "bad:" << split(data) << endl;
+  cout << data << endl;
+  return 0;
+}
+*/
+
+vector<string> split_special(string s){
+  // split string function for csv files that may have commas inside of double quotation marks!
+  vector<string> ret;
+  if(s.size() == 0) return ret;
+  int start_pos = 0;
+  int end_pos = 0; // left-inclusive indices of selection (right index is not inclusive)
+  bool inside_quotes = false;
+  char c = s[end_pos];
+  while(end_pos < s.size() - 1){
+    while(c != ',' && c != '\"' && end_pos < s.size() -1){
+      c = s[++end_pos];
+    }
+    // hit a comma, a double-quotes mark, or got to the end
+    if(c == ','){
+      if(!inside_quotes){
+        if(end_pos > start_pos){
+          str add(s.substr(start_pos, end_pos - start_pos));
+          trim(add, '\"');
+          std::replace(add.begin(), add.end(), ',', ';');
+          ret.push_back(add);
+        }
+        else{
+          ret.push_back(str(""));
+        }
+        start_pos = ++end_pos;
+        c = s[end_pos];
+      }
+      else{
+        c = s[++end_pos];
+      }
+    }
+    else if(c == '\"'){
+      inside_quotes = inside_quotes?false:true;
+      c = s[++end_pos];
+    }
+    else{
+      // out of bounds
+      str add(s.substr(start_pos, end_pos - start_pos + 1));
+      trim(add, '\"');
+      std::replace(add.begin(), add.end(), ',', ';');
+      ret.push_back(add);
+      return ret;
+    }
+  }
+  return ret;
+}
+
 /*
 e.g.:
 str join_(vector<str> d){
