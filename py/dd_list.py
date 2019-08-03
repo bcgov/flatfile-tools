819bdbf1d5176a4b9acd5d87ca00055cbedcdeca
ashlinrichardson <richardson.ashlin@gmail.com>
2019-08-02 20:59:21 -0700

    increase automation
    increase automation

--- py/dd_list.py
+++ py/dd_list.py
@@ -18,7 +18,23 @@ current interfaces:
 import os
 import sys
 import csv
+import time
 from misc import *
+
+# write R script to use readxl library to open the XLS file
+open("dd_unpack.R", "wb").write('''# convert Excel files to CSV: one CSV per sheet in Excel file 20190213
+# usage: Rscript dd_unpack.R [name of Excel file to unpack]
+library(readxl)
+
+fn<-""
+args = commandArgs(trailingOnly=TRUE)
+
+if(length(args)>=1) fn<-args[1]
+if(length(args)==0) stop("Error: usage: Rscript dd_unpack.R [data dictionary file.xls/xlsx]")
+
+for(s in excel_sheets(fn)){ out_fn<-paste(paste(fn, s, sep="_"), ".csv", sep=""); write.csv(read_excel(fn, sheet=s, skip=1), out_fn); }''')
+
+# a = os.system("rscript dd_unpack.R " + fn_druglist)
 
 def run(cmd):
     print cmd
@@ -26,16 +42,14 @@ def run(cmd):
     if a != 0:
         err("command failed: [" +str(cmd) + "]")
 
-
 if os.path.exists('./dd'):
-    err("data dictionary folder already initialized.")
+    print("data dictionary folder already initialized")
+    sys.exit(1)
 
 csv.register_dialect('my', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True)
 whoami = os.popen("whoami").read().strip()
 os.system("mkdir dd")
 
-# this script should be modified to match listed files, to the dd that are in the same folders!
-# e.g., have a for loop that runs over all the folders!
 
 excel = []
 xls = os.popen('find R:/DATA/ -name "data_dictionary*.xls"').read().strip().split("\n")
@@ -50,7 +64,7 @@ print "copying excel files into dd/ folder.."
 for xlf in excel:
     if os.path.exists(xlf):
         date_str = xlf.split('/')[2].strip().replace(" ", "_")
-        run("cp -v " + xlf + " dd/" + date_str + "_" + xlf.split('/')[-1])
+        run("cp " + xlf + " dd/" + date_str + "_" + xlf.split('/')[-1])
 
 print "extracting data dictionaries"
 
@@ -65,6 +79,28 @@ for xlf in excel:
 
     # need to update this line:
     dd_unpack = os.path.abspath("R:/" + whoami + "/bin/R/dd_unpack.R").replace('\\','/')
-    run("rscript " + dd_unpack + " " + localf)
+    a = os.system("rscript dd_unpack.R " + localf)
+    # run("rscript " + dd_unpack + " " + localf)
 
-run("dd_clean")
+run("dd_clean") # only need to clean data dictionaries we didn't make ourselves
+
+pssg = os.popen('find R:/DATA/ -name "pssg_linked*.txt"').readlines()
+for fn in pssg:
+    fn = fn.strip()
+    if not os.path.exists(fn):
+        continue
+    t = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(fn)))
+    new_fn = 'dd/' + t + '_' + fn.split('/')[-1]
+    run('cp ' + fn + ' ' + new_fn)
+    run('dd_convert_pssg_summary_to_csv2 ' + new_fn)
+
+nflt = os.popen('find R:/DATA/ -name "*nflt"').read().strip().split("\n")
+for fn in nflt:
+    if not os.path.exists(fn):
+        continue
+    fn = fn.strip()
+    date_str = fn.split('/')[2].strip().replace(" ", "_")
+    new_fn = "dd/" + date_str + "_" + fn.split('/')[-1]
+    run("cp " + fn + " " + new_fn)
+    run("dd_convert_nflt_to_csv2 " + new_fn)
+
