b3809cddd518e82711df77bbda29937be33de101
ashlinrichardson <richardson.ashlin@gmail.com>
2019-08-02 21:00:17 -0700

    update
    update

--- py/count_studyid.py
+++ py/count_studyid.py
@@ -1,42 +1,50 @@
 import os
 import sys
+from misc import *
+
+debug = False
+
+if len(sys.argv) < 2:
+    err("count_studyid [csv filename]")
 
 def count_id(fn):
     f = open(fn)
-    ci = 0
-    idc = -1
-    count = {}
-    count_count = {}
+    ci = 0 # line index
+    idc = -1 # col. index of studyid field
+    count, count_count = {}, {}
+
+    fields = f.readline().strip().split(",")
+    ci += 1
+
+    for i in range(0, len(fields)):
+        if fields[i].lower() == "studyid":
+            idc = i
+
     while True:
         line = f.readline()
-        if not line:
+        if not line: 
             break
-        if ci ==0:
-            words = line.strip().split(",")
-            for i in range(0, len(words)):
-                if words[i]=="STUDYID" or words[i]=="studyid":
-                    idc = i
-        else:
-            words = line.strip().split(",")
-            id = words[idc]
-            if id in count:
-                count[id] += 1
-            else:
-                count[id] = 1
+        words = line.strip().split(",")
+
+        id = words[idc]
+        if id not in count:
+            count[id] = 0
+        count[id] += 1
         ci += 1
-        #if ci > 1000:
-        #    break
 
     for id_ in count:
-        if count[id_] > 1:
+        if debug and (count[id_] > 1):
             print id_, count[id_]
 
         c = count[id_]
-        if c in count_count:
-            count_count[c] += 1
-        else:
-            count_count[c] = 1
+        if c not in count_count:
+            count_count[c] = 0
+        count_count[c] += 1
 
-    print "count of counts", str(count_count)
+    print "studyid count, instances of count"
+    for c in count_count:
+        print str(c) + "," + str(count_count[c])
+    print "number of distinct studyid,", str(len(count.keys()))
 
+filename = sys.argv[1]
 count_id(filename)
