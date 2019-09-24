2deaa0b809bbfe02fe4e452834b8bf4c661dbf79
ashlinrichardson <richardson.ashlin@gmail.com>
2019-09-24 12:44:59 -0700

    update
    update

--- cpp/csv_select.cpp
+++ cpp/csv_select.cpp
@@ -109,80 +109,80 @@ int main(int argc, char ** argv){
     size_t f_siz = fsize(file_names[j]);
 
     while(getline(f_f, s)){
-	    //f_f.getline(s)){
-      trim(s);
-      lower(s);
-      const char * s_c = s.c_str();
-      words = split(s);
-      if(l_i == 0){
-        nf = words.size();
-        int selected = false;
-        /* make sure for this file, we can match the name of the appropriate field */
-        for0(i, words.size()){
-          trim(words[i]);
-          vector<str> ws(split(words[i], '.'));
-          if(ws.size() > 1){
-            if(ws[ws.size() - 1] == s_f_name){
-              s_f_i = i;
-              selected = true;
+      //f_f.getline(s)){
+        trim(s);
+        lower(s);
+        const char * s_c = s.c_str();
+        words = split(s);
+        if(l_i == 0){
+          nf = words.size();
+          int selected = false;
+          /* make sure for this file, we can match the name of the appropriate field */
+          for0(i, words.size()){
+            trim(words[i]);
+            vector<str> ws(split(words[i], '.'));
+            if(ws.size() > 1){
+              if(ws[ws.size() - 1] == s_f_name){
+                s_f_i = i;
+                selected = true;
+              }
+            }
+            else{
+              if(words[i] == s_f_name){
+                s_f_i = i;
+                cout << words << endl;
+                cout << "SELECTED_FIELD_INDEX (outfile): " << s_f_i << endl;
+                selected = true;
+              }
             }
           }
-          else{
-            if(words[i] == s_f_name){
-              s_f_i = i;
-              cout << words << endl;
-              cout << "SELECTED_FIELD_INDEX (outfile): " << s_f_i << endl;
-              selected = true;
-            }
-          }
-        }
-        if(!selected) err("failed to find selected field"); // this should happen before we do the work.
+          if(!selected) err("failed to find selected field"); // this should happen before we do the work.
 
-        /* write the header */
-        fwrite(s_c, strlen(s_c), 1, outf);
-        fwrite(s_c, strlen(s_c), 1, out2); // 20190310 add support for "excluded" set
-      }
-      else{
-        /* qc or data integrity checking. should have specific scripts to do this. */
-        if(words.size() != nf){
-          words = split_special(s);
-          if(words.size() != nf){
-            cout << words << endl;
-            cout << "Unexpected number of fields (" << words.size() << ") at line number: " << l_i << " expected #: " << nf << endl;
-            err("Remediation failed: wrong number of fields");
-          }
-        }
-        trim(words[s_f_i]);
-        if(false && words[s_f_i].length() != 10){
-          cout << words << endl;
-          cout << "line number: " << l_i << endl;
-          err("wrong number of digits");
-        }
-        if(id.count(words[s_f_i]) > 0){
-          fprintf(outf, "\n%s", s_c);
-          matches_this_iter = true;
-          n_matches ++;
+          /* write the header */
+          fwrite(s_c, strlen(s_c), 1, outf);
+          fwrite(s_c, strlen(s_c), 1, out2); // 20190310 add support for "excluded" set
         }
         else{
-          fprintf(out2, "\n%s", s_c);
+          /* qc or data integrity checking. should have specific scripts to do this. */
+          if(words.size() != nf){
+            words = split_special(s);
+            if(words.size() != nf){
+              cout << words << endl;
+              cout << "Unexpected number of fields (" << words.size() << ") at line number: " << l_i << " expected #: " << nf << endl;
+              err("Remediation failed: wrong number of fields");
+            }
+          }
+          trim(words[s_f_i]);
+          if(false && words[s_f_i].length() != 10){
+            cout << words << endl;
+            cout << "line number: " << l_i << endl;
+            err("wrong number of digits");
+          }
+          if(id.count(words[s_f_i]) > 0){
+            fprintf(outf, "\n%s", s_c);
+            matches_this_iter = true;
+            n_matches ++;
+          }
+          else{
+            fprintf(out2, "\n%s", s_c);
+          }
+
         }
-
+        if((++l_i) % 100000 ==0){
+          size_t p = f_f.tellg();
+          cout << "%" << 100. * float(p) / float(f_siz) << " match %" << ceil(100. * float(n_matches) / float(l_i -1)) << endl;
+          matches_this_iter = false;
+        }
       }
-      if((++l_i) % 100000 ==0){
-        size_t p = f_f.tellg();
-        cout << "%" << 100. * float(p) / float(f_siz) << " match %" << ceil(100. * float(n_matches) / float(l_i -1)) << endl;
-        matches_this_iter = false;
-      }
+      f_f.close();
+      fclose(outf);
+      fclose(out2); // 20190310 added support for excluded / deselected set
     }
-    f_f.close();
-    fclose(outf);
-    fclose(out2); // 20190310 added support for excluded / deselected set
+
+    cout << "size of filter id set:" << id.size() << endl;
+    //cout << "size of filter id set that matched target set: " << id_linked.size() << endl;
+    cout << "number of matches: " << n_matches << endl;
+    set<const char *>::iterator it;
+
+    return 0;
   }
-
-  cout << "size of filter id set:" << id.size() << endl;
-  //cout << "size of filter id set that matched target set: " << id_linked.size() << endl;
-  cout << "number of matches: " << n_matches << endl;
-  set<const char *>::iterator it;
-
-  return 0;
-}
